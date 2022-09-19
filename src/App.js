2e17ff0d68fc9555266bf71223af74fa88a77615
al-jabir <aljabir098@gmail.com>
2022-09-19 19:19:58 +0600

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1>I will be developer</h1>
-      <div className="container card">K</div>
+      <div className="container card">King will be mafia</div>
     </div>
   );
 }
