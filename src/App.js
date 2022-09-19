3ff9c197b35a249fed539209a72d599fc5c3cc2c
al-jabir <aljabir098@gmail.com>
2022-09-19 19:38:06 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <h1>I will be developer</h1>
-      <div className="container card">King will be mafia</div>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
